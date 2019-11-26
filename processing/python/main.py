2d58d738b2fa3d4f77755953de03b43e1c08a50f
Ersin <ersinpw@gmail.com>
2019-11-26 16:16:08 +0100

    Update main.py

--- processing/python/main.py
+++ processing/python/main.py
@@ -7,77 +7,108 @@ from threading import Semaphore
 
 class DialogFlowSampleApplication(Base.AbstractApplication):
     dialog_list = []
+    name = ""
 
     def yaml_open(self, dialog_name):
 
-      print("\033[1;35;40m [!] \x1B[0m playing: "+dialog_name)
+        # Open yaml file
+        print("\033[1;35;40m [!] \x1B[0m running: "+dialog_name)
+        with open(r'dialogs/'+dialog_name) as file:
 
-      with open(r'dialogs/'+dialog_name) as file:
-        output = yaml.load(file, Loader=yaml.FullLoader)
+            # load yaml file
+            output = yaml.load(file, Loader=yaml.FullLoader)
 
-        self.talk(self.get_input(output, "pre_talk"))
+            # nao pre talk
+            self.talk(self.get_input(output, "pre_talk"))
 
-        name		= self.get_input(output, "name")
-        name = name.replace("_", "")
+            # get name, this should correspond to dialogname
+            name		= self.get_input(output, "name")
+            name_filt   = name.replace("_", "")
 
-        DialogFlowSampleApplication.dialog_list.append(name)
-        listen_timeout	= self.get_input(output, "listen_timeout")
-        lock_timeout	= self.get_input(output, "lock_timeout")
+            DialogFlowSampleApplication.dialog_list.append(name)
 
-        print("\033[1;35;40m [!] \x1B[0m name: "+name)
-
-        setattr(self, name, None)
-        setattr(self, name+"Lock", Semaphore(0))
-
-        self.setEyeColour("blue")
-        # self.askreadstoryLock = Semaphore(0)
+            # load timeouts
+            listen_timeout	= self.get_input(output, "listen_timeout")
+            lock_timeout	= self.get_input(output, "lock_timeout")
 
 
+            print("\033[1;35;40m [-] \x1B[0m name: "+name)
 
-        self.setAudioContext(name)
-        self.startListening()
-        print(name)
-        # self, name+'Lock'
+            # declare variables
+            setattr(self, name_filt, None)
+            setattr(self, name_filt+"Lock", Semaphore(0))
 
-        # print(self.askreadstoryLock.acquire(timeout=listen_timeout))
+            self.setAudioContext(name)
 
-        result = getattr(self, name+'Lock')
-        # re(timeout=listen_timeout)
-        print(result.acquire(timeout=listen_timeout))
-        # self.new_lock = name+"Lock"
-        # self.new_lock.acquire(timeout=listen_timeout)
-        self.stopListening()
+            # set eyecolour to blue, since nao is in listen mode
+            self.setEyeColour("blue")
+            self.startListening()
 
-        if not getattr(self, name):
-            print(result.acquire(timeout=lock_timeout))
-          # self.new_lock.acquire(timeout=lock_timeout)
+            # lock mode for audio
+            result = getattr(self, name_filt+'Lock')
+            print(result.acquire(timeout=listen_timeout))
 
-        print(self.get_input(output, "input_none"))
-        print(self.get_input(output, "input_max_tries"))
+            self.stopListening()
 
-        robot_input = getattr(self, name)
-        self.setEyeColour("white")
-        
-        if "input_success" in output:
-          regex_match = self.get_input(output["input_success"], "match")
 
-          if robot_input:
-              print("match", regex_match)
-              print("input", robot_input)
-              match = re.match(regex_match, robot_input)
+            print("\033[1;35;40m [-] \x1B[0m response: "+getattr(self, name_filt))
 
-              if match:
-                  print(self.get_input(output["input_success"][True], "return"))
-                  print(self.get_goto(self.get_input(output["input_success"][True], "goto")))
-              else:
-                  print(self.get_input(output["input_success"][False], "return"))
-                  print(self.get_goto(self.get_input(output["input_success"][False], "goto")))
+            if not getattr(self, name_filt):
+                print(result.acquire(timeout=lock_timeout))
 
-          else:
-              print(self.get_input(output["input_success"][False], "return"))
-              print(self.get_goto(self.get_input(output["input_success"][False], "goto")))
 
-        self.speechLock.acquire()
+            print(self.get_input(output, "input_none"))
+            print(self.get_input(output, "input_max_tries"))
+
+            robot_input = DialogFlowSampleApplication.name
+            print("robot_input", robot_input)
+            self.setEyeColour("white")
+
+            if "input_success" in output:
+                regex_match = self.get_input(output["input_success"], "match")
+
+                if robot_input:
+                  print("match", regex_match)
+                  print("input", robot_input)
+                  match = re.match(regex_match, robot_input)
+
+                  if match:
+                      print("\033[1;35;40m [*] \x1B[0m match 1 ")
+
+                      # Play gesture
+                      self.play_gesture(self.get_input(output["input_success"][True], "gesture"))
+
+                      # Talk
+                      self.talk(self.get_input(output["input_success"][True], "return"))
+
+                      # Go To next dialog or call a method
+                      print(self.get_goto(self.get_input(output["input_success"][True], "gesture")))
+                  else:
+                      print("\033[1;35;40m [*] \x1B[0m match 2 ")
+
+                      # Play gesture
+                      self.play_gesture(self.get_input(output["input_success"][False], "gesture"))
+
+                      # Talk
+                      self.talk(self.get_input(output["input_success"][False], "return"))
+
+                      # Go To next dialog or call a method
+                      print(self.get_goto(self.get_input(output["input_success"][False], "goto")))
+            else:
+                print("\033[1;35;40m [*] \x1B[0m match 2 ")
+
+                # Play gesture
+                self.play_gesture(self.get_input(output["input_success"][False], "gesture"))
+
+                # Talk
+                self.talk(self.get_input(output["input_success"][False], "return"))
+
+                # Go To next dialog or call a method
+                print(self.get_goto(self.get_input(output["input_success"][False], "goto")))
+
+            self.speechLock.acquire()
+            DialogFlowSampleApplication.name = False
+
 
     def get_goto(self, goto):
         if re.search("(yaml)$", goto):
@@ -85,6 +116,19 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
         else:
           if goto in dir(os):
             globals()[goto]()
+
+    def play_gesture(self, gesturename):
+
+        print("\033[1;35;40m [-] \x1B[0m gesture start: "+name)
+
+        self.gestureLock = Semaphore(0)
+
+        print("\033[1;35;40m [-] \x1B[0m gesture name: "+gesturename)
+
+        self.doGesture(gesturename)
+        self.gestureLock.acquire()
+
+        print("\033[1;35;40m [-] \x1B[0m gesture stop")
 
     def get_input(self, arr, name):
         try:
@@ -100,10 +144,11 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
         self.langLock.acquire()
 
         # Dialogflow
-        self.setDialogflowKey("keyfile.json")
+        self.setDialogflowKey("config/keyfile.json")
         self.setDialogflowAgent("sleepy-gbdtuq")
 
-        self.get_goto("answer_name.yaml")
+        # name of yaml file, inside dialogs directory
+        self.get_goto("meeting_robot.yaml")
 
     def talk(self, message):
         self.speechLock = Semaphore(0)
@@ -123,8 +168,12 @@ class DialogFlowSampleApplication(Base.AbstractApplication):
         print("intentname", intentName)
 
         if intentName in DialogFlowSampleApplication.dialog_list and len(args) > 0:
-            self[intentName] = args[0]
-            self[intentName+"Lock"].release()
+            # save in DialogFlowSampleApplication.name
+            DialogFlowSampleApplication.name = args[0]
+
+            # lock nameLock
+            lockfunction = getattr(self, intentName.replace("_", "")+"Lock")
+            lockfunction.release()
 
 
 # Run the application
